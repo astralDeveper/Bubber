0336f177af7dd13f028ffd8e88558f8659146df3
Astral Developer <astraldevelopers0@gmail.com>
2024-07-31 23:20:09 +0500

    Premium bag fix

--- src/screens/app/Premium.js
+++ src/screens/app/Premium.js
@@ -6,167 +6,177 @@ import {
   Dimensions,
   ScrollView,
 } from 'react-native';
-import React from 'react';
-import {X} from '../../assets/Images';
-import {Image} from 'react-native';
+import React, { useState } from 'react';
+import { X } from '../../assets/Images';
+import { Image } from 'react-native';
+import { Toast } from 'react-native-toast-notifications';
 
-const Premium = ({navigation}) => {
+const Premium = ({ navigation }) => {
+  const [packageName, setPackageName] = useState('');
+
   return (
     <SafeAreaView
       style={{
         flex: 1,
         backgroundColor: '#FFF',
       }}>
-        <ScrollView>
-      <View
-        style={{
-          flex: 1,
-        }}>
-        <TouchableOpacity
+      <ScrollView>
+        <View
           style={{
-            margin: 20,
-          }}
-          onPress={() => {
-            navigation.pop();
+            flex: 1,
           }}>
-          <X />
-        </TouchableOpacity>
-        <View>
-          <Text
-            style={{
-              color: '#33E0CF',
-              fontSize: 30,
-              fontWeight: '800',
-              alignSelf: 'center',
-              marginVertical: 20,
-            }}>
-            Get Premium
-          </Text>
-        </View>
-        <Image
-          source={require('../../assets/Images/PNG/Pre.png')}
-          style={{
-            alignSelf: 'center',
-            marginVertical: 20,
-          }}
-        />
-        <View>
-          <Text
-            style={{
-              color: '#000',
-              width: width * 0.7,
-              alignSelf: 'center',
-              textAlign: 'justify',
-            }}>
-            Unlock the ability to chat with up to 5 to 10 people at once by
-            upgrading to premium!
-          </Text>
-        </View>
-        <View>
           <TouchableOpacity
             style={{
-              backgroundColor: '#F3F4F7',
-              padding: 15,
-              flexDirection: 'row',
-              alignItems: 'center',
-              justifyContent: 'space-between',
-              width: width * 0.8,
-              borderRadius: 20,
-              alignSelf: 'center',
-              borderWidth: 2,
-              borderColor: '#3EC8BF',
-              marginVertical: 20,
+              margin: 20,
+            }}
+            onPress={() => {
+              navigation.pop();
             }}>
+            <X />
+          </TouchableOpacity>
+          <View>
             <Text
               style={{
-                color: '#4C4C4C',
-                fontSize: 18,
-                fontWeight: '600',
+                color: '#33E0CF',
+                fontSize: 30,
+                fontWeight: '800',
+                alignSelf: 'center',
+                marginVertical: 20,
               }}>
-              Monthly
+              Get Premium
             </Text>
+          </View>
+          <Image
+            source={require('../../assets/Images/PNG/Pre.png')}
+            style={{
+              alignSelf: 'center',
+              marginVertical: 20,
+            }}
+          />
+          <View>
             <Text
               style={{
                 color: '#000',
-                fontSize: 30,
-                fontWeight: '800',
+                width: width * 0.7,
+                alignSelf: 'center',
+                textAlign: 'justify',
               }}>
-              Rs.199
+              Unlock the ability to chat with up to 5 to 10 people at once by
+              upgrading to premium!
             </Text>
-          </TouchableOpacity>
+          </View>
+          <View>
+            <TouchableOpacity
+              style={{
+                backgroundColor: packageName == 'Monthly' ? '#33E0CF' : '#F3F4F7',
+                padding: 15,
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                width: width * 0.8,
+                borderRadius: 20,
+                alignSelf: 'center',
+                borderWidth: 2,
+                borderColor: '#3EC8BF',
+                marginVertical: 20,
+              }}
+              onPress={() => setPackageName('Monthly')}
+            >
+              <Text
+                style={{
+                  color: packageName == 'Monthly' ? '#EFFF' : '#4C4C4C',
+                  fontSize: 18,
+                  fontWeight: '600',
+                }}>
+                Monthly
+              </Text>
+              <Text
+                style={{
+                  color: '#000',
+                  fontSize: 30,
+                  fontWeight: '800',
+                }}>
+                Rs.199
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                backgroundColor: packageName == 'Annual' ? '#33E0CF' : '#F3F4F7',
+                padding: 15,
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                width: width * 0.8,
+                borderRadius: 20,
+                alignSelf: 'center',
+                borderWidth: 2,
+                borderColor: '#3EC8BF',
+                marginBottom: 20,
+              }}
+              onPress={() => setPackageName('Annual')}
+            >
+              <Text
+                style={{
+                  color: packageName == 'Annual' ? '#EFFF' : '#4C4C4C',
+                  fontSize: 18,
+                  fontWeight: '600',
+                }}>
+                Annual
+              </Text>
+              <Text
+                style={{
+                  color: '#000',
+                  fontSize: 30,
+                  fontWeight: '800',
+                }}>
+                Rs.999
+              </Text>
+            </TouchableOpacity>
+          </View>
           <TouchableOpacity
+            onPress={() => {
+              if (packageName !== '') {
+                Toast.show('You are premium member now');
+                navigation.pop()
+              }else navigation.pop()
+            }}
             style={{
-              backgroundColor: '#F3F4F7',
+              backgroundColor: '#33E0CF',
               padding: 15,
-              flexDirection: 'row',
               alignItems: 'center',
-              justifyContent: 'space-between',
+              borderRadius: 100,
               width: width * 0.8,
-              borderRadius: 20,
               alignSelf: 'center',
-              borderWidth: 2,
-              borderColor: '#3EC8BF',
-              marginBottom: 20,
+              // marginVertical: 40,
             }}>
             <Text
               style={{
-                color: '#4C4C4C',
-                fontSize: 18,
-                fontWeight: '600',
+                color: '#FFF',
+                fontSize: 25,
+                fontFamily: 'ABeeZee-Italic',
               }}>
-              Annual
-            </Text>
-            <Text
-              style={{
-                color: '#000',
-                fontSize: 30,
-                fontWeight: '800',
-              }}>
-              Rs.999
+              Subscribe
             </Text>
           </TouchableOpacity>
+          <View>
+            <Text
+              style={{
+                color: '#33E0CF',
+                width: width * 0.9,
+                alignSelf: 'center',
+                textAlign: 'justify',
+                fontSize: 10,
+                marginTop: 10,
+              }}>
+              By placing this order, you agree to the Terms of Service and Privacy
+              Policy. Subscription automatically renews unless auto-renew is
+              turned off at least 24-hours before the end of the current period.
+            </Text>
+          </View>
         </View>
-        <TouchableOpacity
-          onPress={() => {
-            // navigation.navigate("Interset")
-          }}
-          style={{
-            backgroundColor: '#33E0CF',
-            padding: 15,
-            alignItems: 'center',
-            borderRadius: 100,
-            width: width * 0.8,
-            alignSelf: 'center',
-            // marginVertical: 40,
-          }}>
-          <Text
-            style={{
-              color: '#FFF',
-              fontSize: 25,
-              fontFamily: 'ABeeZee-Italic',
-            }}>
-            Subscribe
-          </Text>
-        </TouchableOpacity>
-        <View>
-          <Text
-            style={{
-              color: '#33E0CF',
-              width: width * 0.9,
-              alignSelf: 'center',
-              textAlign: 'justify',
-              fontSize: 10,
-              marginTop: 10,
-            }}>
-            By placing this order, you agree to the Terms of Service and Privacy
-            Policy. Subscription automatically renews unless auto-renew is
-            turned off at least 24-hours before the end of the current period.
-          </Text>
-        </View>
-      </View>
       </ScrollView>
     </SafeAreaView>
   );
 };
-const {height, width} = Dimensions.get('window');
+const { height, width } = Dimensions.get('window');
 export default Premium;
